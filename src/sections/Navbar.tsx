6d8192fad05a0b6c67fec082cfa4426cc39ba1c1
Akash Singh <94307629+Akshuc@users.noreply.github.com>
2023-08-06 08:10:39 +0530

    Update Navbar.tsx

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -123,7 +123,7 @@ function Navbar() {
               delay: 0.6,
             }}
           >
-            <Button text="Resume" link="http://localhost:8000/VivekRavula..pdf" />
+            <Button text="Resume" link="http://localhost:3000/VivekRavula..pdf" />
           </motion.div>
         </div>
       </div>
